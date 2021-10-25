13252c020a59470fbb30780eadf1c6a630d237de
Yauheni Charniauski <yauheni.charniauski@oracle.com>
2021-10-25 23:21:34 +0300

    init project

---
+++ src/main/java/com/pet/dostavochka/DostavochkaApplication.java
@@ -0,0 +1,11 @@
+package com.pet.dostavochka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DostavochkaApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DostavochkaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/pet/dostavochka/DostavochkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pet.dostavochka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DostavochkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
