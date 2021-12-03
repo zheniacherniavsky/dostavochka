c29b0b324cb49a36f7338ea1df9d157d4f3bfa31
Yauheni Charniauski <zheniacherniavskyplay@gmail.com>
2021-12-03 22:06:57 +0300

    updated sign up view

--- src/main/resources/static/js/signup.js
+++ src/main/resources/static/js/signup.js
@@ -1,6 +1,7 @@
 function signup() {
     const login = document.querySelector('#login').value;
     const password = document.querySelector('#password').value;
+    const resultMessage = document.querySelector('#resultMessage');
 
     fetch("/api/v1/auth/signup", {
         method: "POST",
@@ -11,10 +12,12 @@ function signup() {
         })
     }).then(async (response) => {
         if(response.status === 201) {
-            console.log('Account has been created!');
+            resultMessage.className = "successMessage";
+            resultMessage.innerHTML = 'Account has been created!';
         } else {
             const data = await response.json();
-            console.log(data.errors[0].message);
+            resultMessage.className = "errorMessage";
+            resultMessage.innerHTML = data.errors[0].message;
         }
     })
 }
